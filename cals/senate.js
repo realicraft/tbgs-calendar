d635300f0cb01aca6f6a3655b2bcc4f3706a3d77
realicraft (pi) <39655846+realicraft@users.noreply.github.com>
2021-07-19 21:10:17 -0700

    update calendars

--- cals/senate.js
+++ cals/senate.js
@@ -1,5 +1,6 @@
 //[month, day, year, title, color]
 calendars["senate"] = [
     "Senate Votes",
-    [7, 21, 2021, "Amendments", "green"]
+    [7, 21, 2021, "Amendments", "green"],
+    [7, 22, 2021, "Controversioal Topics", "green"]
 ]
